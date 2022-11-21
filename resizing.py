1b33aefc91ed3c8393b9d1bb3794553524411f0d
Moein Azimi <118739728+moeinazimi@users.noreply.github.com>
2022-11-21 20:40:31 +0000

    Delete resizing.py

--- resizing.py
+++
@@ -1,8 +0,0 @@
-import cv2
-import os 
-img = cv2.imread("spec.png")
-dim = (224,224)
-print(img.shape)
-resized = cv2.resize(img,dim,interpolation = cv2.INTER_NEAREST)
-print(resized.shape)
-cv2.imwrite("spec-resize.png", resized)
